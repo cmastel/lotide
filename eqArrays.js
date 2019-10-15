3ca38935ce64002235e45b166617729d4e1bf1d2
Chris Mastel <chris.mastel@gmail.com>
2019-10-15 21:14:53 +0000

    created eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  // first check if arrays are equal length
+  if (array1.length === array2.length) {
+    // iterate through each value in the (equal length) arrays
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false; // return false as soon as one pair does not match
+      }
+    }
+    return true; // return true if the for loop is able to finish (i.e. no unmatched pairs)
+  } else {
+    // if arrays are not equal length, return false
+    return false;
+  }
+};
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(['One', 'Ring', 'Rule'], ['One', 'Ring', 'Rule']), true);
