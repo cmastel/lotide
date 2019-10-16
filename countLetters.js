ac86ee4e32a4801b7baaa26493b02c566380f658
Chris Mastel <chris.mastel@gmail.com>
2019-10-16 19:43:48 +0000

    remove counting spaces, add comments

--- countLetters.js
+++ countLetters.js
@@ -7,15 +7,18 @@ const assertEqual = function(actual, expected) {
 };
 
 const countLetters = function(stringToCount) {
+  // inputs: stringToCount --> any type of string
   let letterCount = {}
 
+  // iterate through each item in stringToCount
   for (letter of stringToCount) {
-    if (letterCount[letter]) {
-      letterCount[letter] += 1;
-    } else {
-      letterCount[letter] = 1;
+    if (letter !== " ") { // don't count spaces
+      if (letterCount[letter]) {
+        letterCount[letter] += 1; // increase count of existing letter
+      } else {
+        letterCount[letter] = 1; // add new letter to letterCount, set at 1
+      }
     }
-
   }
 
   return letterCount;
@@ -26,8 +29,6 @@ const countLetters = function(stringToCount) {
 const trial1 = "lighthouse in the house";
 
 const trialCount = countLetters(trial1);
-
-console.log(trialCount);
 
 assertEqual(trialCount['l'], 1);
 assertEqual(trialCount['i'], 2);
