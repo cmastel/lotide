b3fa7ca9353bd51fe715adcbd2ea14e6ecd989e4
Chris Mastel <chris.mastel@gmail.com>
2019-10-16 21:29:32 +0000

    added eqArrays functionality

--- eqObjects.js
+++ eqObjects.js
@@ -6,6 +6,22 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(array1, array2) {
+  // first check if arrays are equal length
+  if (array1.length === array2.length) {
+    // iterate through each value in the (equal length) arrays
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false; // return false as soon as one pair does not match
+      }
+    }
+    return true; // return true if the for loop is able to finish (i.e. no unmatched pairs)
+  } else {
+    // if arrays are not equal length, return false
+    return false;
+  }
+};
+
 const eqObjects = function(object1, object2) {
 // returns true if both objects have identical keys with identical values
 // if not, returns false
@@ -13,7 +29,12 @@ const eqObjects = function(object1, object2) {
   // check if objects are the same length
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (let key1 in object1) { // iterate through each key in object1
-      if (object1[key1] !== object2[key1]) {
+      
+      if (Array.isArray(object1[key1])) { // check if value of key1 is an array
+        if (!eqArrays(object1[key1], object2[key1])) { // if so, check if arrays are equal
+          return false; // return false if they are not
+        }
+      } else if (object1[key1] !== object2[key1]) {
         return false; // return false if the key:value pairs do not match
       }
     }
@@ -34,3 +55,13 @@ assertEqual(eqObjects(ab, abc), false);
 
 const ac = { a: '1', c: '3' };
 assertEqual(eqObjects(ab, ac), false);
+
+const cd = { c: '1', d: ['2', 3]};
+const dc = { d: ['2', 3], c: '1'}
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: '1', d: ['2', 3, 4]};
+assertEqual(eqObjects(cd, cd2), false);
+
+const cd3 = {c: '1', d: '4' };
+assertEqual(eqObjects(cd, cd3), false);
