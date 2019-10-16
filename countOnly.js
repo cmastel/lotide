4ae5c59a26d1f6e9652f1d24f5a76d263c4abc1f
Chris Mastel <chris.mastel@gmail.com>
2019-10-16 16:46:58 +0000

    added for loop to count all items

--- countOnly.js
+++ countOnly.js
@@ -10,7 +10,11 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    console.log(item);
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
   }
 
   return results;
