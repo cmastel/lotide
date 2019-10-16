ebe908dc5f6b51f707a8e920453fb12fdc96e0f2
Chris Mastel <chris.mastel@gmail.com>
2019-10-16 16:59:14 +0000

    finalized function, and added comments

--- countOnly.js
+++ countOnly.js
@@ -7,19 +7,31 @@ const assertEqual = function(actual, expected) {
 };
 
 const countOnly = function(allItems, itemsToCount) {
+  // inputs: allItems --> array of items
+  //         itemsToCount --> object with keys that match to items in allItems,
+  //                          and true or false as the vale signifying whether
+  //                          it should be counted
+  // returns: results --> object with keys that are items which are being counted,
+  //                      and the number of occurences in allItems as the value
+  
   const results = {};
 
+  // iterate through allItems
   for (const item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   }
 
   return results;
 };
 
+
+// TEST CODE
 const firstNames = [
   "Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe"
 ];
@@ -29,3 +41,4 @@ const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": t
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
+assertEqual(result1["Salima"], undefined);
