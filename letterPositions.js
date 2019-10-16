4eed9c374801ca9cc4c31e6fbb9b9e94027420f3
Chris Mastel <chris.mastel@gmail.com>
2019-10-16 20:15:30 +0000

    ignore spaces, and add comments

--- letterPositions.js
+++ letterPositions.js
@@ -25,15 +25,18 @@ const eqArrays = function(array1, array2) {
 const letterPositions = function(sentence) {
   let results = {};
 
+  // iterate through each item in sentence
+  // i keeps track of the current index
   for (let i = 0; i < sentence.length; i++) {
-    let letter = sentence[i];
-    if (results[letter]) {
-      results[letter].push(i);
-    } else {
-      results[letter] = [i];
+    let letter = sentence[i]; // assign specifc item to letter variable
+    if (letter !== " ") { // ignore spaces
+      if (results[letter]) {
+        results[letter].push(i); // push index position to array
+      } else {
+        results[letter] = [i]; // create new array with index
+      }
     }
   }
-  console.log("results:", results);
   return results;
 };
 
@@ -42,7 +45,6 @@ const letterPositions = function(sentence) {
 
 const testString = 'hello';
 const testPosition = letterPositions(testString);
-console.log("testPosition:", testPosition);
 
 assertArraysEqual(testPosition['h'], [0]); 
 assertArraysEqual(testPosition['e'], [1]);
