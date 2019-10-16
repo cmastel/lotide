f794c55e744dd538ec0a71f0d64a171be000832b
Chris Mastel <chris.mastel@gmail.com>
2019-10-16 20:31:44 +0000

    added functionality to function

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,7 +7,13 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKeyByValue = function(object, value) {
-  let result = '';
+  let result; // define empty result so that it is undefine if no match found
+
+  for (var genre in object) { // loop through the object
+    if (object[genre] === value) {
+      result = genre;
+    }
+  }
 
   return result;
 };
