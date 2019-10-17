0e5c9644f952253c38c4013a249f943cc0ef43ce
Chris Mastel <chris.mastel@gmail.com>
2019-10-17 20:13:02 +0000

    set up empty function and test code

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const findKey = function(object, callback) {
+  let result = '';
+
+  return result;
+}
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+
+let result1 = findKey({
+  "Blue Hill": {stars: 1},
+  "Akaleri": {stars: 3},
+  "noma": {stars: 2},
+  "elBulli": {stars: 3},
+  "Ora": {stars: 2},
+  "Akelarre": {stars: 3},
+}, x => x.stars === 2);
+
+console.log(result1);
+assertEqual(result1, 'noma');
