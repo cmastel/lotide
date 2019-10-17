0eb1ffdeb770978e3ed1636b6ae31ae605017b4c
Chris Mastel <chris.mastel@gmail.com>
2019-10-17 20:37:20 +0000

    finalized function

--- findKey.js
+++ findKey.js
@@ -1,6 +1,16 @@
+// findKey scans the object, and returns the first key for which
+// the callback returns a truthy value
+// if no key is found, it returns undefined
 const findKey = function(object, callback) {
-  let result = '';
+  let result; // will return undefined if no key found
 
+  for (var item in object) { // loop through the object
+    if (callback(object[item])) { 
+      // if object[item] matches the callback function
+      result = item; 
+      return result;
+    }
+  }
   return result;
 }
 
@@ -23,6 +33,15 @@ let result1 = findKey({
   "Ora": {stars: 2},
   "Akelarre": {stars: 3},
 }, x => x.stars === 2);
-
-console.log(result1);
 assertEqual(result1, 'noma');
+
+console.log('---');
+
+let result2 = findKey({
+  key1: 'blue',
+  key2: 'red',
+  key3: 'green',
+  key4: 'red'
+}, x => x === 'red');
+console.log(result2);
+assertEqual(result2, 'key2');
