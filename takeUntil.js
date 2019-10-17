c905a0331c710ec27f91f97388b786c1fcd63b44
Chris Mastel <chris.mastel@gmail.com>
2019-10-17 19:59:58 +0000

    basic working functionality

--- takeUntil.js
+++ takeUntil.js
@@ -2,8 +2,15 @@
 // the beginning. It should keep going until the callback returns
 // a truthy value.
 const takeUntil = function(array, callback) {
+  const results = [];
 
+  let n = 0;
+  while (!callback(array[n])) {
+    results.push(array[n]);
+    n++;
+  }
 
+  return results;
 }
 
 
