d373854eeaf6644dc4831347d9e322ecde69bef2
Chris Mastel <chris.mastel@gmail.com>
2019-10-17 20:04:01 +0000

    added assert functionality

--- takeUntil.js
+++ takeUntil.js
@@ -5,6 +5,8 @@ const takeUntil = function(array, callback) {
   const results = [];
 
   let n = 0;
+  // use while loop to iterate through array until
+  // the callback function returns false
   while (!callback(array[n])) {
     results.push(array[n]);
     n++;
@@ -14,14 +16,38 @@ const takeUntil = function(array, callback) {
 }
 
 
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🚨🚨🚨 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  // first check if arrays are equal length
+  if (array1.length === array2.length) {
+    // iterate through each value in the (equal length) arrays
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false; // return false as soon as one pair does not match
+      }
+    }
+    return true; // return true if the for loop is able to finish (i.e. no unmatched pairs)
+  } else {
+    // if arrays are not equal length, return false
+    return false;
+  }
+};
+
 // TEST CODE
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
