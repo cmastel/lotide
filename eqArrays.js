fa5c869f6b143a4962e2d162b4e91c9d598c1f6c
Chris Mastel <chris.mastel@gmail.com>
2019-10-18 21:03:56 +0000

    updated to with recursive functionality

--- eqArrays.js
+++ eqArrays.js
@@ -8,19 +8,24 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
   // first check if arrays are equal length
-  if (array1.length === array2.length) {
-    // iterate through each value in the (equal length) arrays
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false; // return false as soon as one pair does not match
-      }
-    }
-    return true; // return true if the for loop is able to finish (i.e. no unmatched pairs)
-  } else {
-    // if arrays are not equal length, return false
+  if (array1.length !== array2.length) {
     return false;
   }
-};
+    
+  // iterate through each value in the (equal length) arrays
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i])) {
+      let check = eqArrays(array1[i], array2[i]);
+      if (check === false) {
+        return false;
+      }
+    } else if (array1[i] !== array2[i]) {
+      return false; // return false as soon as one pair does not match
+    }
+  }
+  return true; // return true if the for loop is able to finish (i.e. no unmatched pairs)
+}; 
+  
 
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
@@ -34,5 +39,5 @@ assertEqual(eqArrays(['One', 'Ring', 'Rule'], ['One', 'Ring', 'Rule']), true);
 
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]) , false);// => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]) , false);// => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);// => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);// => false
